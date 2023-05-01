f74c2ae32af42c03309f61b36d99bdb061b2dca8
CuriousFreeper <90611478+CuriousFreeper@users.noreply.github.com>
2023-05-01 07:46:16 +0200

    Fixed some visual stuff and broken links

--- scripts/lang.js
+++ scripts/lang.js
@@ -7,12 +7,16 @@ var language = {
         animCategory: "3D animations",
         otherCategory: "Other personnal creations"
 
+        
+
     },
     fr:{
 
         aboutCategory: "A propos de moi",
         animCategory: "Animations 3D",
-        otherCategory: "Autres créations personnelles"
+        otherCategory: "Autres créations personnelles",
+
+        aboutSection: "Je suis actuellement en 1ère année de BUT en Métiers du Multimédia et de l'Internet à l'IUT de Mulhouse<br>J'adore créer, apprendre et me découvrir de nouvelles passions. Je suis quelqu'un d'ambitieux et de créatif, qui n'hésite pas à faire de ses rêves une réalité<br>À travers ce portfolio, vous allez retrouver toutes les créations que j'ai pu réaliser, que ce soit dans le cadre de mes études ou en dehors."
 
     },
     ro:{
@@ -43,6 +47,7 @@ function changeLanguage(){
             document.querySelector(".about").innerText = language.fr.aboutCategory;
             document.querySelector(".animation").innerText = language.fr.animCategory;
             document.querySelector(".other").innerText = language.fr.otherCategory;
+            document.querySelector(".aboutSection>p").innerHTML = language.fr.aboutSection;
             break;
         case 2:
             document.querySelector(".about").innerText = language.ro.aboutCategory;
